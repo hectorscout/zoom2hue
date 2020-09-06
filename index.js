5c170565662c6f82c17227df114b9976665a71ba
The Batman <hectorscout@hotmail.com>
2020-09-06 14:38:16 -0600

    handle a get request

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.get('/', (req,res) => {
+    console.log({get: 'get', body:req.body})
+    res.send({ack: 'ack', message: 'it was a get'})
+})
+
 app.post('/', (req, res)  => {
     // const { event } = req.body;
     console.log({body: req.body});
